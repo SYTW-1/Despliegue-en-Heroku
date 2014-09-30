92a1ad997c30abb6cd92e112643cefb44d32ace9
Néstor Álvarez Díaz <nad.9205@gmail.com>
2014-09-30 14:49:53 +0100

    Funcion users ampliada, control de errores

--- twitter.rb
+++ twitter.rb
@@ -9,11 +9,20 @@ class PopularTwitter
 
 	def users(username,followers)
 		client = my_twitter_client()
-		return client.friends(username,{}).take(followers).map{|i| [i.name,i.followers_count]}.sort_by{|i,j| -j}.to_json
+		if followers.to_i <= 0
+			return [["error",0],["description",'Número incorrecto de usuarios']].to_json
+		elsif followers.to_i > 10
+			return [["error",1],["description","Limite de usuarios"]].to_json
+		else
+			return client.friends(username,{}).take(followers.to_i).map{|i| [i.name,i.followers_count]}.sort_by{|i,j| -j}.to_json
+		end
 	end
 
 end
 
-#pop = PopularTwitter.new
-#puts pop.users('naddiaz92',5)
+get "/" do
+end
 
+post "/:username/:followers" do
+	return PopularTwitter.new.users(params[:username],params[:followers])
+end
